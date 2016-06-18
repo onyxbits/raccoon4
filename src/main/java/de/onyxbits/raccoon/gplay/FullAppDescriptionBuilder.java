9e54cef08e9000689a8159baa3b9e8ad43c59743
patrick <github@onyxbits.de>
2016-06-18 17:38:37 +0200

    cleanup

--- src/main/java/de/onyxbits/raccoon/gplay/FullAppDescriptionBuilder.java
+++ src/main/java/de/onyxbits/raccoon/gplay/FullAppDescriptionBuilder.java
@@ -18,7 +18,6 @@ package de.onyxbits.raccoon.gplay;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
